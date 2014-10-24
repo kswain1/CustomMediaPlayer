617073ce14d80cd7610b1beea88bad202f109a11
kswain1 <kehlinswain@yahoo.com>
2014-10-23 22:40:20 -0400

    IOS application
    Short tutorial for a Custom Media Player

--- CustomMediaPlayer/ViewController.h
+++ CustomMediaPlayer/ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
 
 @interface ViewController : UIViewController
+@property(nonatomic, strong) MPMoviePlayerController *moviePlayer; 
 
 @end
--- CustomMediaPlayer/ViewController.m
+++ CustomMediaPlayer/ViewController.m
@@ -13,11 +13,28 @@
 @end
 
 @implementation ViewController
+@synthesize moviePlayer;
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *url = [[NSURL alloc]initWithString:@"http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v"];
+    //Create movie player
+    moviePlayer = [[MPMoviePlayerController alloc]initWithContentURL:url];
+    [moviePlayer.view setFrame:CGRectMake(40, 100, 380, 150)];
+    
+    [self.view addSubview:moviePlayer.view];
+    
+    
+    //some additional customization
+    moviePlayer.fullscreen = YES;
+    moviePlayer.allowsAirPlay = YES;
+    moviePlayer.shouldAutoplay = YES;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
